976b0fad1a1468195e01cfa9da007def62bc9384
Jaeho Yang <ho4121e@gmail.com>
2022-08-19 17:08:25 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [list, setList] = useState([]);
+
+  const get = () => {
+    axios.get('http://43.200.104.40/get',{
+      params: {
+        korName: "박민선",
+        initial: "PMS",
+        birthDay: "20001125",
+        nameMeaning: ['옥돌', '베풀'],
+      }
+    })
+      .then(res => {
+        console.log(res.data);
+        setList(res.data);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={get}>버튼</button>
+      {list.map((e, index)=> (
+        <p key={index}>{e[0]} / {e[1]}</p>
+      ))}
     </div>
   );
 }
